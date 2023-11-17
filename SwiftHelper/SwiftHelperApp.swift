7e95b7b0453b8faae27ffe63c694dfbbeefee989
Aarish Khanna <aarishkhanna@gmail.com>
2023-11-17 23:37:16 +0530

    Initial Commit

---
+++ SwiftHelper/SwiftHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHelperApp.swift
+//  SwiftHelper
+//
+//  Created by Aarish Khanna on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
