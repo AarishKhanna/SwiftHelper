5af695922e7105a4f9907b5cf46249bb1f1636f3
Aarish Khanna <aarishkhanna@gmail.com>
2023-11-25 01:08:54 +0530

    closure capturing concept and retain cycles

--- SwiftHelper/SwiftBasics.playground/Contents.swift
+++ SwiftHelper/SwiftBasics.playground/Contents.swift
@@ -23,3 +23,80 @@ let age2 = example.changeAge() // this will give error if example is let constan
 
 print(age2)
 print(example.age)
+
+
+// concept of capturing of things inside closure and related strong reference retain cycle posibilites:
+
+//func makeIncrementer(incrementAmount: Int) -> () -> Int {
+//    var total = 0
+//
+//    let incrementer: () -> Int = {
+//        total += incrementAmount
+//        return total
+//    }
+//
+//    total += 10
+//
+//    return incrementer
+//}
+//
+//let incrementByTwo = makeIncrementer(incrementAmount: 2)
+//
+//print(incrementByTwo())  // Prints 2
+//print(incrementByTwo())  // Prints 4
+//print(incrementByTwo())  // Prints 6
+ 
+func makeIncrementer(incrementAmount: Int) -> () -> Int {
+    var total = 0
+ 
+    let incrementer: () -> Int = { [total] in
+        // Here, 'total' is captured in the closure
+        // with a capture list to create a copy.
+        var capturedTotal = total
+        capturedTotal += incrementAmount
+        return capturedTotal
+    }
+ 
+    return incrementer
+}
+ 
+let incrementByTwo = makeIncrementer(incrementAmount: 2)
+ 
+print(incrementByTwo())  // Prints 2
+print(incrementByTwo())  // Prints 2 (captured value is not modified)
+ 
+class ObjectA {
+    var completionHandler: (() -> Void)?
+ 
+    deinit {
+        print("ObjectA deinitialized")
+    }
+}
+ 
+class ObjectB {
+    weak var objectA: ObjectA?
+ 
+    deinit {
+        print("ObjectB deinitialized")
+    }
+}
+ 
+var objectA: ObjectA? = ObjectA()
+var objectB: ObjectB? = ObjectB()
+ 
+objectA?.completionHandler = { [weak objectB] in
+    guard let objectB = objectB else {
+        return
+    }
+ 
+    print("Closure executed")
+    objectB.objectA = objectA
+}
+ 
+// The following line is not needed here
+// objectB?.objectA = objectA
+ 
+// Setting both references to nil to deallocate the objects
+objectA = nil
+objectB = nil
+ 
